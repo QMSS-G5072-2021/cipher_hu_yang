c05f0b90c695458eb139ccdb8bdc244669b963b8
Maggiey01 <90772815+Maggiey01@users.noreply.github.com>
2021-11-08 16:19:49 -0500

    add docstrings to cypher function

--- src/cipher_yh3395/cipher_yh3395.py
+++ src/cipher_yh3395/cipher_yh3395.py
@@ -1,4 +1,30 @@
 def cipher(text, shift, encrypt=True):
+     """
+    Encrypt and decrypt letter text by using the Caesar cipher.
+
+    Parameters
+    ----------
+    text : string
+      A string only contains letters from alphabet
+    shift : int
+      A number that you wish to replace each original letter to the letter with that number of positions down the alphabet.
+    encrypt: boolean
+      A boolean value with true indicates encrypt the text and false indicates decrypt the text
+    
+    Returns
+    -------
+    string
+      The encrypt or decrypt letter text.
+
+    Examples
+    --------
+    >>> from cipher_yh3395 import cipher
+    >>> cipher('abc', 1)
+    'bcd'
+    >>> cipher('bcd', 1, False)
+    'abc
+    """
+        
     alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     new_text = ''
     for c in text:
@@ -10,3 +36,4 @@ def cipher(text, shift, encrypt=True):
             new_index %= len(alphabet)
             new_text += alphabet[new_index:new_index+1]
     return new_text
+
